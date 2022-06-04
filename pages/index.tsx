183846a51d6812bfea556942d2bd7d6cf1843600
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2022-06-04 22:55:48 +0700

    Init from the repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,58 +6,59 @@ import Link from 'next/link';
 import { posts } from '../lib/notion'
 
 export async function getServerSideProps() {
-    // Get the posts
-    let { results } = await posts();
-    // Return the result
-    return {
-        props: {
-            posts: results,
-            
-        }
+  // Get the posts
+  let { results } = await posts();
+  // Return the result
+  console.log(results);
+  return {
+    props: {
+      posts: results,
+
     }
+  }
 }
 
 interface Props {
-    posts: [any]
+  posts: [any]
 }
 
 const Home: NextPage<Props> = (props) => {
-    console.log(props.posts)
-    return (
-        <div className={styles.container}>
-        <Head>
-            <title>Latest posts</title>
-        </Head>
+  console.log(props.posts)
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Latest posts</title>
+      </Head>
 
-        <main className={styles.main}>
-            <h1 className={styles.title}>
-            Latest posts
-            </h1>
-            {
-                props.posts.map((result,index) => {
-                return (
-                    <div className={styles.cardHolder} key={index}>
-                    <Link href={`/posts/${result.id}`}>
-                        <Image src={result.cover.external.url}  width={300} height={200} />
-                    </Link>
-                    <div className={styles.cardContent}>
-                        <Link href={`/posts/${result.id}`}>
-                        <a className={styles.cardTitle}>{
-                        result.properties.Name.title[0].plain_text
-                        }</a>
-                        </Link>
-                    </div>
-                    </div>
-                    )
-                })
-            }
-        </main>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Latest posts
+        </h1>
+        {
+          props.posts.map((result, index) => {
+            return (
+              <div className={styles.cardHolder} key={index}>
+                <Link href={`/posts/${result.id}`}>
+                  <Image src={result.cover.external.url} width={300} height={200} />
+                </Link>
+                <div className={styles.cardContent}>
+                  <Link href={`/posts/${result.id}`}>
+                    <a className={styles.cardTitle}>
+                      {result.properties?.Title?.title[0]?.plain_text}
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            )
+          })
+        }
+      </main>
 
-        <footer className={styles.footer}>
-            <p>Blog application</p>
-        </footer>
-        </div>
-    )
+      <footer className={styles.footer}>
+        <p>Blog application</p>
+      </footer>
+    </div>
+  )
 }
 
 export default Home
