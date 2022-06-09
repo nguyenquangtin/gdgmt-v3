08e97f919caf627734533adfe5726f41e6269269
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2022-06-09 10:14:28 +0700

    feat: blog add the _document

--- src/_documents.js
+++ src/_documents.js
@@ -1,31 +1,39 @@
-// import fs from 'fs'
-// import Document from "next/document";
-// import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// export default class MyDocument extends Document {
-//   static async getInitialProps(ctx) {
-//     const sheet = new ServerStyleSheet();
-//     const originalRenderPage = ctx.renderPage;
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* eslint-disable-next-line */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Staatliches"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/uikit@3.10.1/dist/css/uikit.min.css"
+          />
+          <script
+            async
+            src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"
+          />
+          <script
+            async
+            src="https://cdn.jsdelivr.net/npm/uikit@3.10.1/dist/js/uikit-icons.min.js"
+          />
+          <script
+            async
+            src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-//     try {
-//       ctx.renderPage = () =>
-//         originalRenderPage({
-//           enhanceApp: (App) => (props) =>
-//             sheet.collectStyles(<App {...props} />),
-//         });
-
-//       const initialProps = await Document.getInitialProps(ctx);
-//       return {
-//         ...initialProps,
-//         styles: (
-//           <>
-//             {initialProps.styles}
-//             {sheet.getStyleElement()}
-//           </>
-//         ),
-//       };
-//     } finally {
-//       sheet.seal();
-//     }
-//   }
-// }
+export default MyDocument;
